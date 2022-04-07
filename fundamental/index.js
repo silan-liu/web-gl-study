85b03dc233b97088f57364e805a53933c112eabd
liusilan2 <liusilan2@joyy.com>
2022-04-07 10:43:11 +0800

    init

---
+++ fundamental/index.js
@@ -0,0 +1,126 @@
+/**
+ * 创建着色器
+ * @param {*} gl 
+ * @param {*} type 着色器类型
+ * @param {*} source 着色器源码
+ * @returns 
+ */
+function createShader(gl, type, source) {
+    let shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+/**
+ * 创建程序
+ * @param {*} gl 
+ * @param {*} vertexShader 顶点着色器
+ * @param {*} fragmentShader 片段着色器
+ * @returns 
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+    let program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+function main() {
+    let canvas = document.getElementById("canvas");
+    let gl = canvas.getContext("webgl");
+    if (!gl) {
+        return;
+    }
+
+    // 取出着色器的代码
+    let vertexShaderSource = document.getElementById("vertex-shader-2d").text;
+    let fragmentShaderSource = document.getElementById("fragment-shader-2d").text;
+
+    // 创建着色器
+    let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    // 创建程序
+    let program = createProgram(gl, vertexShader, fragmentShader);
+
+    // 取出属性
+    let positionAttributeLocation = gl.getAttribLocation(program, "b_position");
+
+    // 创建 buffer
+    let positionBuffer = gl.createBuffer();
+
+    // 将 gl.ARRAY_BUFFER 与 positionBuffer 绑定起来，也就是说 gl.ARRAY_BUFFER 就表示 positionBuffer。
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    // 3 个顶点
+    let positions = [
+        0, 0,
+        0, 0.5,
+        0.7, 0
+    ];
+
+    // 把顶点数据放入 buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // 更新 canvas 大小
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+    // 设置视口
+    // [-1, 1] -> [0, width]
+    // [-1, 1] -> [0, height]
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    // 清除 canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    // 打开属性
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    // 一次拿 2 个
+    const size = 2;
+
+    // 数据类型是 FLOAT
+    const type = gl.FLOAT;
+
+    const normalized = false;
+
+    // 数据间隔
+    const stride = 0;
+
+    // 从开头
+    const offset = 0;
+
+    // 告知 attribute，如何从 buffer 中拿数据
+    gl.vertexAttribPointer(positionAttributeLocation, size, type, normalized, stride, offset);
+
+    // 三角形
+    const primitiveType = gl.TRIANGLES;
+
+    // 执行 3 次顶点着色器
+    const count = 3;
+
+    // 画一个三角形
+    gl.drawArrays(primitiveType, offset, count);
+}
+
+main()
